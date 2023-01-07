bac3da7c381c5fdfe682eaaaa4f34c9d8dbc4075
baozoudashijian <1787009521@qq.com>
2023-01-07 22:11:25 +0800

    redirect输入根目录重定向

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -6,10 +6,12 @@ import { Third } from "../components/welcome/Third";
 import { Welcome } from "../views/Welcome";
 
 export const routes: RouteRecordRaw[] = [
+    {path: '/', redirect: '/welcome'},
     {
         path: '/welcome',
         component: Welcome, 
         children: [
+            {path: '', redirect: '/welcome/1'},
             {path: '1', component: First},
             {path: '2', component: Second},
             {path: '3', component: Third},
