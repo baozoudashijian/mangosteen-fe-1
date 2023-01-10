c225e1c41c34b9ee8fd6d3235150c76addf332b5
baozoudashijian <1787009521@qq.com>
2023-01-10 23:52:19 +0800

    feat: 完成动画

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,5 +1,5 @@
-import { defineComponent } from 'vue';
-import { RouterView } from 'vue-router';
+import { defineComponent, h, Transition, VNode } from 'vue';
+import { RouteLocationNormalizedLoaded, RouterView } from 'vue-router';
 import s from './Welcome.module.scss'
 import logo from '../assets/icons/logo.svg'
 
@@ -11,7 +11,19 @@ export const Welcome = defineComponent({
                     <img src={logo} alt="123" />
                     <h1>山竹记账</h1>
                 </header>
-                <main><RouterView name="main" /></main>
+                <main>
+                    <RouterView name="main">
+                        {({Component: X, route: R} : {Component: VNode, route: RouteLocationNormalizedLoaded}) => 
+                            <Transition 
+                                enterFromClass={s.slide_fade_enter_from}
+                                enterActiveClass={s.slide_fade_enter_active}
+                                leaveToClass={s.slide_fade_leave_to}
+                                leaveActiveClass={s.slide_fade_leave_active}>
+                                {X}
+                            </Transition>
+                        }    
+                    </RouterView>
+                </main>
                 <div class={s.actions}><RouterView name="footer" /></div>
             </div>
         )
