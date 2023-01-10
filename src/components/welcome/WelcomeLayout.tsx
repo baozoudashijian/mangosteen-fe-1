3a8dec89cf22c570890f2a5dc79af72ba613e2da
baozoudashijian <1787009521@qq.com>
2023-01-10 22:03:14 +0800

    feat: 终极重构

--- src/components/welcome/WelcomeLayout.tsx
+++ src/components/welcome/WelcomeLayout.tsx
@@ -1,21 +1,18 @@
-import { defineComponent } from 'vue';
+import { defineComponent, FunctionalComponent } from 'vue';
 import s from './Welcome.module.scss'
 import { RouterLink } from 'vue-router';
 
-export const WelcomeLayout = defineComponent({
-    setup(props, ctx) {
-        const { slots } = ctx
-        return () => (
-            <div class={s.wrapper}>
-                <div class={s.card}>
-                    {slots.icon?.()}
-                    
-                    {slots.title?.()}
-                </div>
-                <div class={s.actions}>
-                    {slots.actions?.()}
-                </div>
+export const WelcomeLayout: FunctionalComponent = (props, ctx) => {
+    const { slots: {icon, title, actions} } = ctx
+    return (
+        <div class={s.wrapper}>
+            <div class={s.card}>
+                {icon?.()}
+                {title?.()}
             </div>
-        )
-    }
-})
+            <div class={s.actions}>
+                {actions?.()}
+            </div>
+        </div>
+    )
+}
