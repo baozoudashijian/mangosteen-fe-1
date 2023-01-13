a315b353752dfd3e3852e3cb737fe6dc043f7ec4
baozoudashijian <1787009521@qq.com>
2023-01-13 22:25:44 +0800

    feat: 定义hooks，使用hooks

---
+++ src/hooks/useSwipe.tsx
@@ -0,0 +1,5 @@
+import { Ref } from "vue";
+
+export const useSwipe = (element: Ref<HTMLElement | null>) => {
+
+}
--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,25 +1,28 @@
-import { defineComponent, h, Transition, VNode } from 'vue';
+import { defineComponent, h, ref, Transition, VNode } from 'vue';
 import { RouteLocationNormalizedLoaded, RouterView } from 'vue-router';
 import s from './Welcome.module.scss'
 import logo from '../assets/icons/logo.svg'
+import { useSwipe } from '../hooks/useSwipe';
 
 export const Welcome = defineComponent({
     setup() {
+        const main = ref<HTMLElement | null>(null)
+        useSwipe(main)
         return () => (
             <div class={s.welcome}>
                 <header>
                     <img src={logo} alt="123" />
                     <h1>山竹记账</h1>
                 </header>
-                <main>
+                <main ref={main}>
                     <RouterView name="main">
-                        {({Component: X, route: R} : {Component: VNode, route: RouteLocationNormalizedLoaded}) => 
+                        {({Component: Content, route: R} : {Component: VNode, route: RouteLocationNormalizedLoaded}) => 
                             <Transition 
                                 enterFromClass={s.slide_fade_enter_from}
                                 enterActiveClass={s.slide_fade_enter_active}
                                 leaveToClass={s.slide_fade_leave_to}
                                 leaveActiveClass={s.slide_fade_leave_active}>
-                                {X}
+                                {Content}
                             </Transition>
                         }    
                     </RouterView>
