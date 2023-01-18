fcb9c87889e8d4b56531e2307e69bf0ef7ea0f18
baozoudashijian <1787009521@qq.com>
2023-01-18 22:20:03 +0800

    feat: MainLayout重构Start页面

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,19 @@
+import { defineComponent } from 'vue';
+import { NavBar } from '../components/navbar/NavBar';
+import s from './MainLayout.module.scss';
+export const MainLayout = defineComponent({
+    setup(props, context) {
+        const { slots } = context
+        return () => (
+            <>
+                <NavBar>
+                    {{
+                        default: slots.text?.(),
+                        icon: slots.icon?.()
+                    }}
+                </NavBar>
+                {slots.default?.()}
+            </>
+        )
+    }
+})
--- src/views/start/Start.tsx
+++ src/views/start/Start.tsx
@@ -7,6 +7,7 @@ import { Icon } from '../../components/icon/Icon';
 import { NavBar } from '../../components/navbar/NavBar';
 import { OverLay } from '../../components/overlay/OverLay';
 import { OverLayList } from '../../components/overlay/OverLay'
+import { MainLayout } from '../../layouts/MainLayout';
 import s from './Start.module.scss'
 
 
@@ -14,9 +15,9 @@ export const Start = defineComponent({
     setup() {
         const visible = ref(false)
         const overLayData = ref<OverLayList>([
-            {icon: 'charts', text: '统计图表'},
-            {icon: 'export', text: '导出数据'},
-            {icon: 'notify', text: '记账提醒'},
+            { icon: 'charts', text: '统计图表' },
+            { icon: 'export', text: '导出数据' },
+            { icon: 'notify', text: '记账提醒' },
         ])
         const onclick = () => {
             console.log(123)
@@ -29,26 +30,31 @@ export const Start = defineComponent({
         }
         return () => (
             <>
-                <NavBar>
+                <MainLayout>
                     {{
-                        default: () => '山竹记账',
-                        icon: () => <Icon name="menu" width="50" height="37.5" onClick={displayOverlay} />
+                        icon: () => <Icon name="menu" width="50" height="37.5" onClick={displayOverlay} />,
+                        text: () => '山竹记账',
+                        default: () => 
+                            <>
+                                <main class={s.main}>
+                                    <Center direction="column">
+                                        <Icon name="pig" />
+                                    </Center>
+                                </main>
+                                <div class={s.button_wrapper}>
+                                    <RouterLink to={'/items'}>
+                                        <Button class={s.button} onClick={onclick}>开始记账</Button>
+                                    </RouterLink>
+                                </div>
+                                <FloatButton onClick={onclick} />
+                                <OverLay visible={visible.value} onClose={closeOverlay} list={overLayData.value} />
+                            </>
+                        
                     }}
-                </NavBar>
-                <main class={s.main}>
-                    <Center direction="column">
-                        <Icon name="pig" />
-                    </Center>
-                </main>
-                <div class={s.button_wrapper}>
-                    <RouterLink to={'/items'}>
-                        <Button class={s.button} onClick={onclick}>开始记账</Button>
-                    </RouterLink>
-                </div>
-                <FloatButton onClick={onclick} />
-                <OverLay visible={visible.value} onClose={closeOverlay} list={overLayData.value}/>
+                </MainLayout>
+
             </>
-            
+
         )
     }
 })
