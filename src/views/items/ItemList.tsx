f50a9143ae407946f7062fd0699a0ca2e79e9221
baozoudashijian <1787009521@qq.com>
2023-02-14 22:35:25 +0800

    feat: 解决更新ref不更新ui的问题，提取出Tags组件，使用v-model实现

--- src/views/items/ItemList.tsx
+++ src/views/items/ItemList.tsx
@@ -1,43 +1,46 @@
-import { defineComponent, ref } from 'vue';
+import { computed, defineComponent, reactive, ref } from 'vue';
 import { Icon } from '../../components/icon/Icon';
 import { InputPad } from '../../components/input_pad/InputPad';
 import { Tab } from '../../components/Tabs/Tab';
 import { Tabs } from '../../components/Tabs/Tabs';
 import { MainLayout } from '../../layouts/MainLayout';
-import { Button } from 'vant';
 import s from './ItemList.module.scss';
+import { Tags } from './Tags';
 
 export const ItemList = defineComponent({
     setup(props, context) {
         const selected = ref('expend')
+        const tag = reactive({
+            id: 0
+        })
 
         const onUpdateSelected = (name: string) => {
-            console.log(name, 'name')
             selected.value = name
         }
+        
         return () => (
             <>
-            <MainLayout>
-                {{
-                    icon: () => <Icon name="left" width="50" height="37.5" fill="#fff" />,
-                    text: () => '记一笔',
-                    default: () => {
-                        return <>
-                            <Tabs selected={selected.value} onUpdateSelected={onUpdateSelected}>
-                                <Tab label='支出' name="expend">
-                                    123
-                                </Tab>
-                                <Tab label='收入' name="income">
-                                    456
-                                </Tab>
-                            </Tabs>
-                            <div class={s.inputPad_wrapper}>
-                                <InputPad />
-                            </div>
-                        </>
-                    }
-                }}
-            </MainLayout>
+                <MainLayout>
+                    {{
+                        icon: () => <Icon name="left" width="50" height="37.5" fill="#fff" />,
+                        text: () => '记一笔',
+                        default: () => {
+                            return <>
+                                <Tabs selected={selected.value} onUpdateSelected={onUpdateSelected}>
+                                    <Tab label='支出' name="expend" class={s.tags_wrapper}>
+                                        <Tags selected={selected.value} v-model:tagId={tag.id} />
+                                    </Tab>
+                                    <Tab label='收入' name="income" class={s.tags_wrapper}>
+                                        <Tags selected={selected.value} v-model:tagId={tag.id} />
+                                    </Tab>
+                                </Tabs>
+                                <div class={s.inputPad_wrapper}>
+                                    <InputPad />
+                                </div>
+                            </>
+                        }
+                    }}
+                </MainLayout>
             </>
         )
     }
---
+++ src/views/items/Tags.tsx
@@ -0,0 +1,77 @@
+import { defineComponent, ref } from 'vue';
+import s from './Tags.module.scss';
+export const Tags = defineComponent({
+    props: {
+        selected: {
+            type: String,
+            required: true
+        },
+        tagId: Number
+    },
+    setup(props, context) {
+        const refExpensesTags = ref([
+            { id: 1, name: '餐费', sign: '￥', category: 'expenses' },
+            { id: 2, name: '打车', sign: '￥', category: 'expenses' },
+            { id: 3, name: '聚餐', sign: '￥', category: 'expenses' },
+            { id: 4, name: '打车', sign: '￥', category: 'expenses' },
+            { id: 5, name: '聚餐', sign: '￥', category: 'expenses' },
+            { id: 6, name: '打车', sign: '￥', category: 'expenses' },
+            { id: 7, name: '聚餐', sign: '￥', category: 'expenses' }
+        ])
+        const refIncomeTags = ref([
+            { id: 8, name: '滴滴', sign: '￥', category: 'income' },
+            { id: 9, name: '彩票', sign: '￥', category: 'income' },
+            { id: 10, name: '滴滴', sign: '￥', category: 'income' },
+            { id: 11, name: '工资', sign: '￥', category: 'income' },
+            { id: 12, name: '彩票', sign: '￥', category: 'income' },
+            { id: 13, name: '滴滴', sign: '￥', category: 'income' },
+            { id: 14, name: '彩票', sign: '￥', category: 'income' },
+            { id: 15, name: '滴滴', sign: '￥', category: 'income' },
+            { id: 16, name: '彩票', sign: '￥', category: 'income' },
+            { id: 17, name: '滴滴', sign: '￥', category: 'income' },
+            { id: 18, name: '工资', sign: '￥', category: 'income' },
+            { id: 19, name: '彩票', sign: '￥', category: 'income' },
+            { id: 20, name: '滴滴', sign: '￥', category: 'income' },
+            { id: 21, name: '彩票', sign: '￥', category: 'income' },
+            { id: 22, name: '滴滴', sign: '￥', category: 'income' },
+            { id: 23, name: '彩票', sign: '￥', category: 'income' },
+            { id: 24, name: '滴滴', sign: '￥', category: 'income' },
+            { id: 25, name: '工资', sign: '￥', category: 'income' },
+            { id: 26, name: '彩票', sign: '￥', category: 'income' },
+            { id: 27, name: '滴滴', sign: '￥', category: 'income' },
+            { id: 28, name: '彩票', sign: '￥', category: 'income' },
+            { id: 29, name: '滴滴', sign: '￥', category: 'income' },
+            { id: 30, name: '彩票', sign: '￥', category: 'income' },
+            { id: 31, name: '滴滴', sign: '￥', category: 'income' },
+            { id: 32, name: '工资', sign: '￥', category: 'income' },
+            { id: 33, name: '彩票', sign: '￥', category: 'income' },
+            { id: 34, name: '滴滴', sign: '￥', category: 'income' },
+            { id: 35, name: '彩票', sign: '￥', category: 'income' },
+            { id: 36, name: '滴滴', sign: '￥', category: 'income' },
+            { id: 37, name: '彩票', sign: '￥', category: 'income' },
+            { id: 38, name: '滴滴', sign: '￥', category: 'income' }
+        ])
+        const selectData: any = {
+            'expend': refExpensesTags,
+            'income': refIncomeTags
+        }
+        const onTagClick = (id: number) => {
+            console.log(id, 'id')
+            context.emit('update:tagId', id)
+        }
+        return () => (<>
+            {selectData[props.selected].value.map((tag: any) => {
+                return <div class={[s.tag, props.tagId === tag.id ? s.selected : '']} onClick={() => onTagClick(tag.id)}>
+                    <div class={s.sign}>
+                        {tag.sign}
+                    </div>
+                    <div class={s.name}>
+                        {tag.name}
+                    </div>
+                </div>
+            })}
+        </>
+
+        )
+    }
+})
